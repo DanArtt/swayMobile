96c4373d290941071a2eedd7e70285c374d28e66
rttwx <162727777+rttwx@users.noreply.github.com>
2025-05-23 18:19:23 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,8 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 export class HomePage implements AfterViewInit {
   @ViewChild('swiper', { static: false }) swiperRef!: ElementRef;
 
+  constructor(private menu: MenuController) {}
+
   ngAfterViewInit() {
     setTimeout(() => {
       const swiperEl = this.swiperRef.nativeElement;
@@ -18,35 +21,39 @@ export class HomePage implements AfterViewInit {
     }, 100); // espera um pouco para garantir que tudo esteja renderizado
   }
 
+  openMenu() {
+    this.menu.enable(true, 'mainMenu');
+    this.menu.open('mainMenu');
+  }
+
   produtos = [
-  {
-    nome: 'Camiseta ThugNine DouhBoy',
-    preco: 179.99,
-    imagem: '../../assets/CAMISETA-DOUGHBOY1.png',
-  },
-  {
-    nome: 'T-Shirt Class Verde',
-    preco: 179.99,
-    imagem: '../../assets/T-SHIRT-CLASS-GREEN.png',
-  },
-  {
-    nome: 'Short Trunda Off-White',
-    preco: 179.99,
-    imagem: '../../assets/SHORT-TUNDRA-OFF-WHITE.png',
-  },
-  {
-    nome: 'Óculos De Sol Dunville',
-    preco: 179.99,
-    imagem: '../../assets/oculos-De-Sol-Dunville.png',
-  },
-  // Adicione mais produtos aqui!
-];
+    {
+      nome: 'Camiseta ThugNine DouhBoy',
+      preco: 179.99,
+      imagem: '../../assets/CAMISETA-DOUGHBOY1.png',
+    },
+    {
+      nome: 'T-Shirt Class Verde',
+      preco: 179.99,
+      imagem: '../../assets/T-SHIRT-CLASS-GREEN.png',
+    },
+    {
+      nome: 'Short Trunda Off-White',
+      preco: 179.99,
+      imagem: '../../assets/SHORT-TUNDRA-OFF-WHITE.png',
+    },
+    {
+      nome: 'Óculos De Sol Dunville',
+      preco: 179.99,
+      imagem: '../../assets/oculos-De-Sol-Dunville.png',
+    },
+    // Adicione mais produtos aqui!
+  ];
 
   activeGroup = 0;
 
-setGroup(index: number) {
-  this.activeGroup = index;
+  setGroup(index: number) {
+    this.activeGroup = index;
+  }
 }
 
-
-}
